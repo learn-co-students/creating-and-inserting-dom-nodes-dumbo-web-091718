6c8ae8c8caae8709b2ed7a1f863cad3d2e5bcfd4
ScottWaring <scott.waring@gmail.com>
2018-11-01 09:22:57 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+let element = document.createElement('div')
+element.innerHtml = 'Hello, Dom!';
+element.style.backgroundColor = '#f9f9f9';
+document.body.appenedChild(element);
+element.style.textAlign = 'center';
